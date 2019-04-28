899f37e4af7bac1763e93407175c30601a081d96
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-04-28 23:21:44 +0530

    Update zone-t.sh

--- zone-t.sh
+++ zone-t.sh
@@ -11,7 +11,7 @@ fi
 
 green="\e[92m"
 red="\e[91m"
-violet="\e[34m"
+blue="\e[34m"
 none="\e[0m"
 
 #working area
@@ -21,9 +21,9 @@ while read line
 do
         zone=$(dig  @${line} ${1}. axfr)
         if echo "$zone" | grep -i "Transfer failed." &>/dev/null ; then
-        echo -e "${red}zone Transfer ${none}${violet}[Failed]${none}${red} in ${line} Server${none}"
+        echo -e "${red}zone Transfer ${none}${blue}[Failed]${none}${red} in ${line} Server${none}"
         else
-        echo -e "${green}zone Transfer ${none}${violet}[SUCCESS]${none}${green} in ${line} Server${none}"
+        echo -e "${green}zone Transfer ${none}${blue}[SUCCESS]${none}${green} in ${line} Server${none}"
         dig  @${line} ${1}. axfr >> zonetranfer_results.md
         echo "DNS Retervied Data is saved in the file zonetranfer_results.md"
         fi
