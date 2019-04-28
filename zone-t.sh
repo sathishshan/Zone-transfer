1f403b033e592a242ab12152336c20fb1d145139
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-04-28 23:11:57 +0530

    Update zone-t.sh

--- zone-t.sh
+++ zone-t.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+#argument check
+
 if [ $# -ne 1 ]; then
-        echo -e "Need one domain to run the script \e[92m[USAGE]:\e[0m $0 naked-domain"
+        echo -e "Need one domain to run the script${green}[USAGE]:${none} $0 naked-domain"
         exit
 fi
 
+#colours
+
+green="\e[92m"
+red="\e[91m"
+violet="\e[34m"
+none="\e[0m"
+
+#working area
 dig ns ${1} +short > ns.txt
 
 while read line
 do
         zone=$(dig  @${line} ${1}. axfr)
         if echo "$zone" | grep -i "Transfer failed." &>/dev/null ; then
-        echo -e "\e[91mzone Transfer\e[0m \e[34m[Failed]\e[0m\e[91m in ${line} Server\e[0m"
+        echo -e "${red}zone Transfer${none}${violet}[Failed]${none}${red} in ${line} Server${none}"
         else
-        echo -e "\e[92mzone Transfer\e[0m \e[34m[SUCCESS]\e[0m\e[92m in ${line} Server\e[0m"
+        echo -e "${green}zone Transfer${none}${violet}[SUCCESS]${none}${green} in ${line} Server${none}"
         dig  @${line} ${1}. axfr >> zonetranfer_results.md
         echo "DNS Retervied Data is saved in the file zonetranfer_results.md"
         fi
