b42f30c76c0bc4e845d72a756795256c3a5af36f
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-04-28 23:16:00 +0530

    Update zone-t.sh

--- zone-t.sh
+++ zone-t.sh
@@ -3,7 +3,7 @@
 #argument check
 
 if [ $# -ne 1 ]; then
-        echo -e "Need one domain to run the script${green}[USAGE]:${none} $0 naked-domain"
+        echo -e "Need one domain to run the script ${green}[USAGE]:${none} $0 naked-domain"
         exit
 fi
 
@@ -21,9 +21,9 @@ while read line
 do
         zone=$(dig  @${line} ${1}. axfr)
         if echo "$zone" | grep -i "Transfer failed." &>/dev/null ; then
-        echo -e "${red}zone Transfer${none}${violet}[Failed]${none}${red} in ${line} Server${none}"
+        echo -e "${red}zone Transfer ${none}${violet}[Failed]${none}${red} in ${line} Server${none}"
         else
-        echo -e "${green}zone Transfer${none}${violet}[SUCCESS]${none}${green} in ${line} Server${none}"
+        echo -e "${green}zone Transfer ${none}${violet}[SUCCESS]${none}${green} in ${line} Server${none}"
         dig  @${line} ${1}. axfr >> zonetranfer_results.md
         echo "DNS Retervied Data is saved in the file zonetranfer_results.md"
         fi
