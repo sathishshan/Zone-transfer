1b8ddb20a7157db5526de9ec5c189de661571117
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-04-28 19:29:32 +0530

    Update zone-t.sh

--- zone-t.sh
+++ zone-t.sh
@@ -5,16 +5,16 @@ if [ $# -ne 1 ]; then
         exit
 fi
 
-dig ns $1 +short > ns.txt
+dig ns ${1} +short > ns.txt
 
 while read line
 do
-        zone=$(dig  @${line} $1 axfr)
+        zone=$(dig  @${line} ${1}. axfr)
         if echo "$zone" | grep -i "Transfer failed." &>/dev/null ; then
         echo -e "\e[91mzone Transfer\e[0m \e[34m[Failed]\e[0m\e[91m in ${line} Server\e[0m"
         else
         echo -e "\e[92mzone Transfer\e[0m \e[34m[SUCCESS]\e[0m\e[92m in ${line} Server\e[0m"
-        dig  @${line} $1 axfr >> zonetranfer_results.md
+        dig  @${line} ${1}. axfr >> zonetranfer_results.md
         echo "DNS Retervied Data is saved in the file zonetranfer_results.md"
         fi
 done < ns.txt
